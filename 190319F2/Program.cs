5b9c4b6ce4728effd0dc9168742564e8538595c2
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-03-19 18:50:21 +0100

    Add project files.

---
+++ 190319F2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _190319F2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
